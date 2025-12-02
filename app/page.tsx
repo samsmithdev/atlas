aec54651a44890dce99040618e15703a41e317c2
Sam Smith <samsmithdev1014@gmail.com>
2025-12-02 12:29:50 -0500

    Added AtlasToolbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import AtlasToolbar from "./_components/Atlas-Toolbar";
 
 export default function Home() {
 
@@ -9,21 +10,7 @@ export default function Home() {
       </header>
       <main className="max-w-full">
 
-        <div className="m-2 border border-indigo-500 p-4 grid grid-flow-col"> {/* This is the "toolbar/project selector" bar */}
-          <div className="m-2 grid grid-flow-col text-xl font-bold border-b border-indigo-500 w-fit shrink"> {/* This is the project selector */}
-            <div className="whitespace-nowrap shrink mr-2">
-              <h2 className="shrink">SD000001 - A.T.L.A.S.</h2>
-            </div>
-            <div className="w-fit ml-2 mr-2">
-              <svg className="w-6 h-6 text-gray-800 dark:text-white shrink" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 9-7 7-7-7" />
-              </svg>
-            </div>
-          </div>
-          <div> {/* This is the toolbar to the right of the project selector */}
-            <p>Hello World!</p>
-          </div>
-        </div>
+        <AtlasToolbar />
 
         <div className="w-full m-2 grid grid-cols-12 gap-4 border"> {/* This is the main editing space, including the file nav and editor */}
           <div className="col-span-4 p-4 rounded mt-4 ml-0 border">
