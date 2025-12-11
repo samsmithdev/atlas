eaf2c99afafa7761def0c6b943e619b7d3fff881
Sam Smith <samsmithdev1014@gmail.com>
2025-12-10 19:53:47 -0500

    Fetched project

--- app/_components/AtlasToolbar.tsx
+++ app/_components/AtlasToolbar.tsx
@@ -1,10 +1,17 @@
+import prisma from '@/app/lib/db';
+
 export default function AtlasToolbar() {
+    const project = await prisma.project.findFirst({
+        where: {
+            name: "Atlas",
+        }
+    });
 
     return (
         <div className="w-full mt-2 mb-2 gap-4 border grid grid-flow-col"> {/* This is the "toolbar/project selector" bar */}
             <div className="m-2 grid grid-flow-col text-xl font-bold border-b border-indigo-500 w-fit shrink"> {/* This is the project selector */}
                 <div className="whitespace-nowrap shrink mr-2">
-                    <h2 className="shrink">SD000001 - A.T.L.A.S.</h2>
+                    <h2 className="shrink">${project.name}</h2>
                 </div>
                 <div className="w-fit ml-2 mr-2">
                     <svg className="w-6 h-6 text-gray-800 dark:text-white shrink" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
