cc2e3fb23128c014e1ff1f20791e405ed106b032
Sam Smith <samsmithdev1014@gmail.com>
2025-12-19 19:40:34 -0500

    Began building fetching files

--- app/actions/files.tsx
+++ app/actions/files.tsx
@@ -4,7 +4,7 @@ import prisma from '@/app/lib/db'
 export async function createFile(newFile: { id?: string, name: string, createdDate?: Date, contents: string, author: string, description: string, tags: string[], projectId?: string | null}) {
   const { id, name, createdDate, author, description, tags, contents } = newFile;
   // This is the logic we are testing
-  const note = await prisma.file.create({
+  const file = await prisma.file.create({
     data: {
       id: id ?? undefined,
       name: name,
@@ -14,7 +14,15 @@ export async function createFile(newFile: { id?: string, name: string, createdDa
       description: description,
       tags: tags,
     }
-  })
+  });
   
-  return note
+  return file
+}
+
+export async function fetchFilesForProject(projectId: string) {
+  const files = await prisma.file.findMany({
+    where: {
+      projectId: projectId,
+    }
+  });
 }
