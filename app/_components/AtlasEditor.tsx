d53b1999b42e1d369495b677639881d3b73b29e7
Sam Smith <samsmithdev1014@gmail.com>
2025-12-24 17:58:11 -0500

    Added "Test" to current test variables

--- app/_components/AtlasEditor.tsx
+++ app/_components/AtlasEditor.tsx
@@ -6,21 +6,21 @@ import { fetchProject } from "@/app/actions/projects"
 import { fileURLToPath } from "url";
 
 export default async function AtlasEditor() {
-    const activeProject = await fetchProject("SD001");
-    const activeFiles = await fetchFilesForProject(activeProject?.id ?? "");
-    const activeFile = activeFiles.find(file => file.id === "SD0");
+    const activeTestProject = await fetchProject("SD001");
+    const activeTestFiles = await fetchFilesForProject(activeTestProject?.id ?? "");
+    const activeTestFile = activeTestFiles.find(file => file.id === "SD0");
     // const userWithId99 = users.find(user => user.id === 99)
-    //const activeFile = project?.files?.find(file => file.id = "SD0");
+    //const activeTestFile = project?.files?.find(file => file.id = "SD0");
 
-    console.log(activeFile);
-    console.log(activeProject);
+    console.log(activeTestFile);
+    console.log(activeTestProject);
 
     return (
         <div className="w-full grid grid-cols-12 gap-4 mx-auto h-[95%] overflow-hidden"> {/* This is the main editing space, including the file nav and editor */}
             <div className="col-span-4 p-4 rounded mt-4 ml-0 border overflow-auto">
                 <ul className="space-y-2 divide-y-3 divide-dashed divide-indigo-500">
-                    {activeFiles.length ? (
-                        activeFiles.map((file) => (
+                    {activeTestFiles.length ? (
+                        activeTestFiles.map((file) => (
                             <li key={file.id}>
                                 {file.name}
                             </li>
@@ -32,10 +32,10 @@ export default async function AtlasEditor() {
             </div>
 
             <div className="col-span-8 mt-4 w-full gap-4 p-4 border rounded overflow-hidden">
-                {activeFile ? (
+                {activeTestFile ? (
                     <AtlasMarkdownEditor
-                        fileId={activeFile.id}
-                        initialContent={activeFile.content} />
+                        fileId={activeTestFile.id}
+                        initialContent={activeTestFile.content} />
                 ) : (
                     <div className="flex items-center justify-center h-full text-gray-400">
                         <p>Please select a file to begin editing...</p>
