af3d3c8a8723d3e8de167aad43a17a890b494232
Sam Smith <samsmithdev1014@gmail.com>
2026-01-01 17:23:49 -0500

    Dynamic structure initially implemented

--- app/projects/[projectId]/layout.tsx
+++ app/projects/[projectId]/layout.tsx
@@ -1,4 +1,4 @@
-// This will be a blank screen for passing things maybe?
+// This will just be a Project navigator in the top left corner of this div
 import { ReactNode } from 'react'
 import { fetchAllProjects, fetchProject } from '@/app/actions/projects'
 import AtlasToolbar from '@/app/_components/AtlasToolbar';
@@ -8,10 +8,20 @@ type LayoutProps = {
     params: { projectId?: string }
 }
 
-export default function ProjectIDBlankPage({ children, params }: LayoutProps) {
+export default async function ProjectIdLayout({ children, params }: LayoutProps) {
+    const awaitedParams = await params;
+
+    const activeProject = awaitedParams.projectId ? (await fetchProject(awaitedParams.projectId)) ?? undefined : (undefined);
+
     return (
-        <div>
-            {children}
+        <div className="grid grid-rows-[auto_1fr] w-full h-full overflow-hidden" id="projectId-layout">
+            <div className="flex w-full bg-gray-800 shrink" id="projects-layout-toolbar-container"> {/* This is the container that will have the Toolbar with project selector... Oh, ToolBar and page content? Duh lol */}
+                <AtlasToolbar activeProject={activeProject} />
+            </div>
+
+            <div className="flex-1 w-full overflow-hidden" id="projects-layout-children-space">
+                {children}
+            </div>
         </div>
     )
 }
--- app/projects/layout.tsx
+++ app/projects/layout.tsx
@@ -5,23 +5,13 @@ import AtlasToolbar from '@/app/_components/AtlasToolbar';
 
 type LayoutProps = {
     children: ReactNode,
-    params: { projectId?: string }
+    params: {}
 }
 
 export default async function ProjectLayout({ children, params }: LayoutProps) {
-    const awaitedParams = await params;
-
-    const activeProject = awaitedParams.projectId ? (await fetchProject(awaitedParams.projectId)) ?? undefined : (undefined);
-
     return (
-        <div className="grid grid-rows-[auto_1fr] w-full h-full" id="projects-layout-container">
-            <div className="flex w-full bg-gray-800 shrink" id="projects-layout-toolbar-container"> {/* This is the container that will have the Toolbar with project selector... Oh, ToolBar and page content? Duh lol */}
-                <AtlasToolbar activeProject={activeProject} />
-            </div>
-
-            <div className="flex-1 h-full w-full overflow-hidden" id="projects-layout-children-space">
+        <div className="w-full h-full" id="projects-layout-container">
                 {children}
-            </div>
         </div>
     )
 }
