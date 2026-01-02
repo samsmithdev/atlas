f62baf7510d1b2ea75d6cfc50a9acdbca405e067
Sam Smith <samsmithdev1014@gmail.com>
2026-01-02 16:57:21 -0500

    Made progress on the full page project navigator

--- app/_components/AtlasProjectPreviewPane.tsx
+++ app/_components/AtlasProjectPreviewPane.tsx
@@ -1,14 +1,30 @@
 // Preview Pane 
 import { fetchProject } from "../actions/projects";
+import { Project } from "../generated/prisma";
 
 export default async function AtlasProjectPreviewPane({ previewProjectId }: { previewProjectId?: string }) {
-    const previewProject = previewProjectId ? (await fetchProject(previewProjectId)) : (undefined);
+    const blankProject: Project = {
+        name: "No Project Selected",
+        id: "-",
+        created_date: new Date(),
+        author: "-",
+        description: "-",
+        subjectId: null
+    }
+
+    const previewProject = previewProjectId ? (await fetchProject(previewProjectId)) ?? blankProject : (blankProject);
 
     return (
-        <div>
-            {previewProject ? 
-            (<p>The project is {previewProject.name}</p>) : 
-            (<p>There is no project selected.</p>)}
+        <div className="flex flex-col mx-auto items-center" id="atlas-project-preview-pane-container">
+            {/* Just one column of fields? */}
+            <ul className="mx-auto" id="atlas-project-preview-pane-list">
+                <li key="name">
+                    <p className="font-bold">Project Name: {previewProject.name}</p>
+                </li>
+                <li key="id">
+                    <p className="font-bold">Project ID: {previewProject.id}</p>
+                </li>
+            </ul>
         </div>
     )
 }
