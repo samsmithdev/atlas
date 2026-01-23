dc1e010c6d5295bb03a0fa4bede2de3c7a9bc073
Sam Smith <samsmithdev1014@gmail.com>
2026-01-23 13:36:33 -0500

    Moved existing components to deprecated folder
    Implemented initial project selector button

---
+++ app/_components/AtlasProjectSelectorButton.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { AtlasGroupedProjectsForNav, AtlasProjectNavigatorItem } from '../_types/AtlasNavigatorTypes';
+import { useRouter, usePathname, useSearchParams } from 'next/navigation';
+
+export default async function AtlasProjectSelectorButton({ projectMenuItems, activeProject } : { projectMenuItems: AtlasGroupedProjectsForNav, activeProject?: AtlasProjectNavigatorItem }) {
+    const router = useRouter();
+    const pathname = usePathname();
+    const searchParams = useSearchParams();
+
+
+    return (
+        <button
+            className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition-colors"
+        >
+            {(activeProject ? (activeProject.name) : ("Please select a project..."))}
+        </button>
+    )
+}
