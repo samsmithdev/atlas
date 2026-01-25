8c86d7a249de0d233b857fe8e9b41a07f3b636e8
Sam <samsmithdev1014@gmail.com>
2026-01-25 16:09:57 -0500

    Added homepage for development

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import AtlasSiteHeader from "./_components/AtlasSiteHeader";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +27,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased h-screen flex flex-col overflow-hidden border`}
       >
-        <AtlasSiteHeader />
         <main className="flex-1 w-full h-full overflow-hidden flex justify-center">{children}</main>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
-import Image from "next/image";
-import AtlasProjectEditor from "./_components/AtlasProjectEditor";
+import Link from "next/link";
 
 export default function Home() {
 
   return (
-    <div className="container w-screen h-[95%] mx-auto border">
-      <AtlasProjectEditor />
+    <div className="bg-slate-900 w-full h-full items-center flex flex-col">
+      <h1>Welcome to the ATLAS Development center!</h1>
+      <Link
+        className="border rounded-2xl border-indigo-600 text-white mx-auto p-4 text-2xl"
+        href="/projects">
+        Open /projects
+      </Link>
     </div>
   );
 }
