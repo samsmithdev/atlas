bf28840ca39e5070a1a763d8e6270fe88ad33940
Sam <samsmithdev1014@gmail.com>
2026-01-25 16:16:43 -0500

    Finished dashboard, restarting the layout stack according to plan

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,9 @@ import Link from "next/link";
 export default function Home() {
 
   return (
-    <div className="bg-slate-900 w-full h-full items-center flex flex-col">
-      <h1>Welcome to the ATLAS Development center!</h1>
+    <div className="bg-slate-900 w-full h-full items-center flex flex-col gap-8">
+      <h1 className="text-5xl text-indigo-200 border-b-4 border-indigo-500 p-8">Welcome to the ATLAS Development Center!</h1>
+      <p className="text-xs text-indigo-200">Unauthorized use is not authorized.</p>
       <Link
         className="border rounded-2xl border-indigo-600 text-white mx-auto p-4 text-2xl"
         href="/projects">
