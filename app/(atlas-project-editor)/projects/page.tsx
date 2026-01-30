966941cdafe552971be420fe3167708db6740572
Sam Smith <samsmithdev1014@gmail.com>
2026-01-30 12:43:21 -0500

    Swapped modal testing page with project selector

--- app/(atlas-project-editor)/projects/page.tsx
+++ app/(atlas-project-editor)/projects/page.tsx
@@ -1,10 +1,33 @@
-'use client';
-
+import AtlasSelectProjectPane from "@/components/atlas/panes/AtlasSelectProjectPane";
 import AtlasActionModalTestPage from "./_components/AtlasActionModalTestPage";
+import { fetchProjectsForMenu } from "@/actions/projects";
+import { AtlasProjectNavigatorItem, AtlasGroupedProjectsForNav } from "@/types/AtlasNavigatorTypes";
 
-export default function ProjectsHomepage() {
-
+export default async function ProjectsHomepage() {
+        const projectLinkSubjectGroup = (await fetchProjectsForMenu()).reduce((accumulator, project) => {
+            const subjectId = project.subject?.id ?? 'uncategorized';
+            const subjectName = project.subject?.name ?? 'MISC';
+            const shortCode = project.subject?.shortcode ?? 'MISC';
+            
+            if (!accumulator[subjectId]) {
+                accumulator[subjectId] = {
+                    subjectName: subjectName,
+                    subjectShortcode: shortCode,
+                    projects: []
+                };
+            }
+    
+            const navigatorItem: AtlasProjectNavigatorItem = {
+                id: project.id,
+                name: project.name,
+                link: `/projects/${project.id}/files`
+            }
+    
+            accumulator[subjectId].projects.push(navigatorItem);
+    
+            return accumulator;
+        }, {} as AtlasGroupedProjectsForNav)
     return (
-        <AtlasActionModalTestPage />
+        <AtlasSelectProjectPane projectsGroupedBySubject={projectLinkSubjectGroup} />
     );
 }
