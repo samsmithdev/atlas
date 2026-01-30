02978e8a4b8c5cbbbe274bfea7e1558156535338
Sam Smith <samsmithdev1014@gmail.com>
2026-01-30 17:06:30 -0500

    Began file navigator

--- types/AtlasNavigatorTypes.tsx
+++ types/AtlasNavigatorTypes.tsx
@@ -6,9 +6,15 @@ export type AtlasProjectNavigatorItem = {
     link: string,
 };
 
-// <SubjectId, AtlasProjectNavigatorItem
+// <SubjectId, AtlasProjectNavigatorItem>
 export type AtlasGroupedProjectsForNav = Record<string, {
     subjectName: string;
     subjectShortcode: string;
     projects: AtlasProjectNavigatorItem[];
 }>;
+
+export type AtlasFileNavigatorItem = {
+    id: string,
+    name: string,
+    link: string
+}
