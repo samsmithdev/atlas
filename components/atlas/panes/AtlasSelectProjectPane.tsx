69ddb87a89134a5ade74ecb5468a510e8abb2179
Sam <samsmithdev1014@gmail.com>
2026-01-30 22:08:01 -0500

    Updated SelectProjectPane's initial CSS

--- components/atlas/panes/AtlasSelectProjectPane.tsx
+++ components/atlas/panes/AtlasSelectProjectPane.tsx
@@ -18,23 +18,23 @@ export default function AtlasSelectProjectPane({ projectsGroupedBySubject, activ
             {Object.values(projectsGroupedBySubject).sort((left, right) => {
                 return ('' + left.subjectShortcode).localeCompare(right.subjectShortcode);
             }).map((subjectGroup) => (
-                <div 
+                <div
                     key={subjectGroup.subjectName}
-                    className="break-inside-avoid mb-6 rounded-lg p-4 border border-gray-100"
+                    className="break-inside-avoid mb-6 p-4 border border-indigo-500"
                     id="atlas-select-project-pane_subject-group-container"
                 >
-                    <div className="flex items-baseline justify-between mb-2 border-b pb-2">
-                        <h3 className="font-semibold text-gray-800">
-                            <span className="text-xs font-mono uppercase">{subjectGroup.subjectShortcode}</span> {subjectGroup.subjectName}
+                    <div className="flex items-baseline justify-between mb-2 border-b pb-2 border-indigo-700">
+                        <h3 className="font-semibold text-gray-100">
+                            <span className="font-bold uppercase">{subjectGroup.subjectShortcode}</span> {subjectGroup.subjectName}
                         </h3>
                     </div>
 
-                    <ul className="space-y-2">
+                    <ul className="">
                         {subjectGroup.projects.map((project) => (
                             <li key={project.id}>
-                                <Link 
+                                <Link
                                     href={project.link}
-                                    className="block text-sm text-gray-600"
+                                    className="block text-sm text-gray-200 hover:bg-indigo-950 cursor-auto hover:cursor-pointer p-2 shrink"
                                 >
                                     {project.name}
                                 </Link>
