b7d0608401bc3c9848f58a935e9e35210a677a0e
Sam Smith <samsmithdev1014@gmail.com>
2026-01-31 13:32:38 -0500

    Styled ile nav

--- components/atlas/panes/AtlasSelectFileForProjectPane.tsx
+++ components/atlas/panes/AtlasSelectFileForProjectPane.tsx
@@ -14,7 +14,7 @@ export default function AtlasSelectFileForProjectPane({ files }: AtlasSelectFile
     return (
         <div className="w-full h-full">
             {files ? (
-                <ul className='space-y-2 grid grid-cols-1 pr-4 pl-4 h-full mb-4'>
+                <ul className='space-y-2 flex flex-col pr-4 pl-4 h-full mb-4'>
                     {files.sort((left, right) => {
                         return left.readableId.localeCompare(right.readableId);
                     })
@@ -23,8 +23,8 @@ export default function AtlasSelectFileForProjectPane({ files }: AtlasSelectFile
                                 key={file.id}
                                 href={file.link}
                                 className={`p-2 rounded text-sm border-2 border-indigo-500 border-bottom ${file.id === activeFileId
-                                    ? 'bg-blue-800 text-blue-200 font-medium'
-                                    : 'text-gray-400 hover:bg-gray-100'
+                                    ? 'bg-blue-800 hover:bg-blue-600 text-blue-200 font-medium'
+                                    : 'text-gray-400 hover:bg-blue-950'
                                     }`}
                             >
                                 {file.readableId}-{file.name}
