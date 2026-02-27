045175c0c78d5ae40c21f5d635d9e0364e296b9c
Sam Smith <samsmithdev1014@gmail.com>
2026-02-27 13:58:33 -0500

    Auto stash before merge of "feat/inbox-and-ux-upgrades" and "origin/feat/inbox-and-ux-upgrades"

---
+++ components/atlas/panels/AtlasEditMarkdownFilePanel.tsx
@@ -0,0 +1,68 @@
+'use client';
+
+import { useEditor, EditorContent } from '@tiptap/react';
+import { useEffect, useState } from 'react';
+import { useDebouncedCallback } from 'use-debounce';
+import StarterKit from '@tiptap/starter-kit'
+import { updateFile } from '@/actions/files'
+
+interface AtlasEditMarkdownFilePanelProps {
+    fileId: string;
+    initialContent: any; // ANY WARNING: Best for tip tap content?
+}
+
+export default function AtlasEditMarkdownFilePanel({ fileId, initialContent }: AtlasEditMarkdownFilePanelProps) {
+    const [saveStatus, setSaveStatus] = useState('Saved');
+
+    const saveToDatabase = async (jsonContent: any) => {
+        setSaveStatus('Saving...');
+        try {
+            await updateFile(fileId, jsonContent);
+            setSaveStatus('Saved');
+        } catch (error) {
+            setSaveStatus('Error saving');
+            console.error(error);
+        }
+    }
+
+    const debouncedSaved = useDebouncedCallback((content) => {
+        saveToDatabase(content);
+    }, 1000);
+
+    useEffect(() => {
+        return () => {
+            debouncedSaved.flush();
+        }
+    }, [debouncedSaved]);
+
+    const editor = useEditor({
+        extensions: [
+            StarterKit,
+        ],
+        content: initialContent,
+        editorProps: {
+            attributes: {
+                class: 'prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto focus:outline-none h-full p-4',
+            },
+        },
+        immediatelyRender: false,
+        onUpdate: ({ editor }) => {
+            setSaveStatus('Unsaved changes...');
+
+            const json = editor.getJSON();
+
+            debouncedSaved(json);
+        }
+    })
+
+    return (
+        <div className="flex flex-col gap-2 h-full w-full pb-4">
+            <div className='text-sm text-gray-500 text-right'>
+                Status: {saveStatus}
+            </div>
+            <div className='border border-gray-300 rounded-md p-4 min-h-[500px] flex-1'>
+                <EditorContent editor={editor} />
+            </div>
+        </div>
+    )
+}
