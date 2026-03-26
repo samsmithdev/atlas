547944e151516231acba331d1d95c677d61f9cd4
Sam Smith <samsmithdev1014@gmail.com>
2026-03-26 19:09:50 -0400

    Added action to page, committing pre-folder update

--- app/(atlas-project-editor)/projects/inbox/page.tsx
+++ app/(atlas-project-editor)/projects/inbox/page.tsx
@@ -1,11 +1,14 @@
 "use server";
 
 import { fetchInboxItemTransaction } from "@/actions/inbox";
+import { fetchSubjectProjectFolderFileSelectors } from "@/actions/subjects";
 import AtlasInboxProcessingPanel from "@/components/atlas/panels/AtlasInboxProcessingPanel";
 import { AtlasInboxItem } from "@/types/AtlasListTypes";
 
 export default async function InboxPage() {
-    const inboxItems = (await fetchInboxItemTransaction())
+    const inboxItems = (await fetchInboxItemTransaction());
+    const fileNavigatorItems = await fetchSubjectProjectFolderFileSelectors();
+
     return (
         <div className="w-full h-full overflow-hidden p-4 flex">
             <AtlasInboxProcessingPanel inboxItems={inboxItems.data ?? []} />
