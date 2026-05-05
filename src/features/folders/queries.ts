70d26ecfb40eb33ddff744ae7baefd0cc0537e40
Sam Smith <samsmithdev1014@gmail.com>
2026-05-05 16:51:01 -0400

    Added initial folder type and fetch

--- src/features/folders/queries.ts
+++ src/features/folders/queries.ts
@@ -1,1 +1,24 @@
-"use server";
+import { withAuth } from "@/lib/action-wrapper";
+import prisma from "@/lib/db";
+import { folderSelectorSelect } from "./types";
+
+export const fetchFolderSelectorsBySubject = withAuth(
+  async (userId, projectId: string) => {
+    const result = await prisma.folder.findMany({
+      where: {
+        userId,
+        projectId,
+      },
+      select: folderSelectorSelect,
+      orderBy: { name: "asc" },
+    });
+
+    const safeFolders = JSON.parse(JSON.stringify(result));
+
+    return {
+      success: true,
+      message: "Folder selectors fetched successfully.",
+      data: safeFolders,
+    };
+  }
+);
