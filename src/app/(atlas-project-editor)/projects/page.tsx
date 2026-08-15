48b38df5beb53e1ec691eff752b2e855b124cc7e
Sam Smith <samsmithdev1014@gmail.com>
2026-08-14 20:13:10 -0400

    Removed basic initial "header"

--- src/app/(atlas-project-editor)/projects/page.tsx
+++ src/app/(atlas-project-editor)/projects/page.tsx
@@ -1,14 +1,10 @@
 import AtlasSelectProjectContainer from "@/features/workspace/components/projects/AtlasSelectProjectPanelContainer";
 import AtlasSelectProjectPanelSkeleton from "@/features/workspace/components/projects/AtlasSelectProjectPanelSkeleton";
-import Link from "next/link";
 import { Suspense } from "react";
 
 export default async function ProjectsHomepage() {
   return (
     <div className="w-full h-full p-2 overflow-hidden">
-      <Link href="/subjects/create">Create Subject Modal?</Link>
-      <Link href="/projects/create">Create Project Modal</Link>
-      <Link href="/projects/select">Select Project Modal</Link>
       <Suspense fallback={<AtlasSelectProjectPanelSkeleton />}>
         <AtlasSelectProjectContainer />
       </Suspense>
