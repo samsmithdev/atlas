b3b56ad7ce01b48db947aa6928509f2686b5b494
Sam Smith <samsmithdev1014@gmail.com>
2026-08-14 20:43:56 -0400

    Added createSubjectProjectDropdown

--- src/app/@header/projects/page.tsx
+++ src/app/@header/projects/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/layout/Header";
 import UserDropdownContainer from "@/features/auth/components/UserDropdown/UserDropdownContainer";
 import UserDropdownSkeleton from "@/features/auth/components/UserDropdown/UserDropdownSkeleton";
+import CreateSubjectProjectDropdown from "@/features/workspace/components/CreateSubjectProjectDropdown";
 import { Suspense } from "react";
 
 export default async function ProjectEditorHeader() {
   return (
     <Header
+      leftNode={<CreateSubjectProjectDropdown />}
       rightNode={
         <Suspense fallback={<UserDropdownSkeleton />}>
           <UserDropdownContainer />
