955a2476fd52627f49cfd601302eb791b357525f
Sam Smith <samsmithdev1014@gmail.com>
2026-08-14 20:21:47 -0400

    Add more headers and fixed /files bug

--- src/app/(atlas-project-editor)/projects/[projectId]/files/page.tsx
+++ src/app/(atlas-project-editor)/projects/[projectId]/files/page.tsx
@@ -1,5 +1,3 @@
 export default async function AtlasProjectFilesPage() {
-  return {
-    /* Empty EditMarkdownPane? */
-  };
+  return <p>hi</p>;
 }
--- src/app/@header/projects/[projectId]/page.tsx
+++ src/app/@header/projects/[projectId]/page.tsx
@@ -1,3 +1,16 @@
+import Header from "@/components/layout/Header";
+import UserDropdownContainer from "@/features/auth/components/UserDropdown/UserDropdownContainer";
+import UserDropdownSkeleton from "@/features/auth/components/UserDropdown/UserDropdownSkeleton";
+import { Suspense } from "react";
+
 export default async function ProjectEditorHeader() {
-  return null;
+  return (
+    <Header
+      rightNode={
+        <Suspense fallback={<UserDropdownSkeleton />}>
+          <UserDropdownContainer />
+        </Suspense>
+      }
+    />
+  );
 }
